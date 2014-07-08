85e2dfa2555edea9f48363b6d2ff6cebd7576823
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-08 20:07:45 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+pagenav
+=======
+
+navigation toolbar for a book
+
+## installation
+
+```
+$ component install binocarlos/pagenav
+```
+
+## example
+
+```js
+var PageNav = require('pagenav')
+var nav = PageNav()
+nav.appendTo(document.querySelector('#nav'))
+
+var pages = [{
+	title:'Page 1'	
+},{
+	title:'Page 2'
+},{
+	title:'Page 3'
+}]
+
+// pages can be an array or just a number
+nav.setPages(pages)
+
+// change the HTML of a nav element
+nav.on('page', function(elem, index){
+	elem.innerHTML = '0' + index
+})
+
+nav.on('click', function(index){
+	// set the book to page 'index'	
+})
+
+// set the highlighted page to 3
+nav.setPage(3)
+```
+
+## api
+
+### `var nav = PageNav()`
+
+Create a new navbar
+
+### `nav.setPages(pages)`
+
+Assign the number of pages for the navbar - pages can be an array or a number
+
+### `nav.setPage(index)`
+
+Set the currently active page of the navbar
+
+## events
+
+### `nav.on('page', function(elem, index){})`
+
+A page element has been rendered
+
+### `nav.on('click', function(index){})`
+
+A page element has been clicked
+
+## css
+
+The DOM structure for the navbar:
+
+```html
+<div class="pagenav-bar">
+	<div class="pagenav-page">1</div>
+	<div class="pagenav-page">2</div>
+	<div class="pagenav-page pagenav-active">3</div>
+	<div class="pagenav-page">4</div>
+</div>
+```
+
+## licence
+MIT
---
+++ index.js
@@ -0,0 +1,68 @@
+var Emitter = require('emitter')
+var classes = require('classes')
+var css = require('css')
+
+module.exports = PageArrows;
+
+var defaults = {
+  width:10,
+  inset:2
+}
+
+function PageArrows (opts) {
+  if (!(this instanceof PageArrows)) return new PageArrows(book, opts);
+  opts = opts || {}
+  Object.keys(defaults || {}).forEach(function(key){
+    if(!opts[key]){
+      opts[key] = defaults[key]
+    }
+  })
+  if(!opts.height){
+    opts.height = opts.width
+  }
+  this._opts = opts
+}
+
+Emitter(PageArrows.prototype)
+
+PageArrows.prototype.createArrow = function (side) {
+  var self = this;
+  var arrow = document.createElement('div') 
+  classes(arrow).add('pagearrows-arrow').add('pagearrows-' + side)
+  arrow.addEventListener('click', function(){
+    self.emit('click', side)
+  })
+  var props = {
+    position:'absolute',
+    width:this._opts.width + '%',
+    height:this._opts.height + '%',
+    top:(50 - (this._opts.height/2)) + '%'
+  }
+  props[side] = inset + '%'
+  css(arrow, props)
+}
+
+PageArrows.prototype.render = function () {
+  if(this._element){
+    return this._element
+  }
+  this._element = document.createElement('div')
+  classes(this._element).add('pagearrows-container')
+  css(this._element, {
+    position:'absolute',
+    width:'100%',
+    height:'100%'
+  })
+  this._arrows = {
+    left:this.createArrow('left'),
+    right:this.createArrow('right')
+  }
+  this._element.appendChild(this._arrows.left)
+  this._element.appendChild(this._arrows.right)
+  return this._element
+}
+
+PageArrows.prototype.appendTo = function (target) {
+  if (typeof target === 'string') target = document.querySelector(target)
+  target.appendChild(this.render())
+}
