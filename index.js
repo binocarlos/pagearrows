6d7b1bb18e8f73fe744a58d54172a9c6896b7e84
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-10 21:50:43 +0000

    remove opts

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ var css = require('css')
 
 module.exports = PageArrows;
 
-var defaults = {
-  width:10,
-  inset:2
-}
-
-function PageArrows (opts) {
-  if (!(this instanceof PageArrows)) return new PageArrows(opts);
-  opts = opts || {}
-  Object.keys(defaults || {}).forEach(function(key){
-    if(!opts[key]){
-      opts[key] = defaults[key]
-    }
-  })
-  if(!opts.height){
-    opts.height = opts.width
-  }
-  this._opts = opts
+function PageArrows () {
+  if (!(this instanceof PageArrows)) return new PageArrows();
 }
 
 Emitter(PageArrows.prototype)
@@ -51,14 +36,6 @@ PageArrows.prototype.createArrow = function (side) {
     self.emit('click', side, directions[side])
   })
   arrow.style.cursor = 'pointer'
-  var props = {
-    position:'absolute',
-    width:this._opts.width + '%',
-    height:this._opts.height + '%',
-    top:(50 - (this._opts.height/2)) + '%'
-  }
-  props[side] = this._opts.inset + '%'
-  css(arrow, props)
   this.emit('render', arrow, side)
   return arrow
 }
